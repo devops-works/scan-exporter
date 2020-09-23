be890bc700cff0985987f44adef7936f64e494c0
eze-kiel <ezekiel@fastmail.com>
2020-09-23 16:00:04 +0200

    read workerCount from WRKCNT env var

--- scan/scan.go
+++ scan/scan.go
@@ -3,6 +3,7 @@ package scan
 import (
 	"fmt"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -11,7 +12,7 @@ import (
 	"github.com/sparrc/go-ping"
 )
 
-var workersCount = 1000
+var workersCount int
 
 // Target holds an IP and a range of ports to scan
 type Target struct {
@@ -108,6 +109,17 @@ func (t *Target) Name() string {
 // Run should be called using `go` and will run forever running the scanning
 // schedule
 func (t *Target) Run() {
+	// Check if workerCount is set in WRKCNT env variable.
+	// If the program is launched as root, the env var must be set by root user
+	if os.Getenv("WRKCNT") == "" {
+		workersCount = 1000
+	} else {
+		num, err := strconv.Atoi(os.Getenv("WRKCNT"))
+		if err != nil {
+			panic(err) // TODO: find a way to handle this
+		}
+		workersCount = num
+	}
 	// Create trigger channel for scheduler
 	trigger := make(chan string, 100)
 
