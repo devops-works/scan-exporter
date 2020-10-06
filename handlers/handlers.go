d948d81fe143a816ac3266b940c1dfcaba37fb0e
eze-kiel <ezekiel@fastmail.com>
2020-10-06 17:45:35 +0200

    renamed default handler to not found

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,18 +8,18 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// HandleFunc handles the metrics path.
+// HandleFunc handles /metrics and not found path.
 func HandleFunc() *mux.Router {
 	r := mux.NewRouter()
 	r.Handle("/metrics", promhttp.Handler())
 
-	r.NotFoundHandler = http.HandlerFunc(defaultPage)
+	r.NotFoundHandler = http.HandlerFunc(notFoundPage)
 
 	return r
 }
 
-// defaultPage set the response header to 404 status and prints an error message.
-func defaultPage(w http.ResponseWriter, r *http.Request) {
+// notFoundPage set the response header to 404 status and prints an error message.
+func notFoundPage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "404 page not found")
 }
