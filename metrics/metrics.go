32a83d0c9572ebeb63cc365eb08e611e09888656
eze-kiel <ezekiel@fastmail.com>
2020-10-06 12:22:28 +0200

    [WIP] handling redis database

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,1 +1,18 @@
 package metrics
+
+type resMsg struct {
+	id              string
+	ip              string
+	protocol        string
+	openPorts       []string
+	unexpectedPorts []string
+	closedPorts     []string
+}
+
+// Handle receives data from a finished scan. It also receive the number of targets declared in config file
+func Handle(res resMsg, nTargets int) {
+	// check if there is already some entries in redis
+	// write data in target:ip:proto:1 if there is something, else in target:ip:proto:0
+	// compare
+	// expose
+}
