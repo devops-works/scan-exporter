fc82c9b66e7073620f4dd86b753e3c84cb6932fb
eze-kiel <ezekiel@fastmail.com>
2020-10-07 14:09:44 +0200

    wrote CompareStringSlices func and tests

--- common/common.go
+++ common/common.go
@@ -9,3 +9,27 @@ func StringInSlice(s string, sl []string) bool {
 	}
 	return false
 }
+
+// CompareStringSlices checks if two slices are equal.
+// It returns the number of different items.
+func CompareStringSlices(sl1, sl2 []string) int {
+	diffCounter := 0
+
+	if len(sl1) <= len(sl2) {
+		for _, val := range sl1 {
+			if !StringInSlice(val, sl2) {
+				diffCounter++
+			}
+		}
+		diffCounter += len(sl2) - len(sl1)
+	} else {
+		for _, val := range sl2 {
+			if !StringInSlice(val, sl1) {
+				diffCounter++
+			}
+		}
+		diffCounter += len(sl1) - len(sl2)
+	}
+
+	return diffCounter
+}
--- common/common_test.go
+++ common/common_test.go
@@ -1,6 +1,8 @@
 package common
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_StringInSlice(t *testing.T) {
 	tests := []struct {
@@ -16,6 +18,39 @@ func Test_StringInSlice(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := StringInSlice(tt.s, tt.sl); got != tt.want {
 				t.Errorf("stringInSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompareStringSlices(t *testing.T) {
+
+	tests := []struct {
+		name string
+		sl1  []string
+		sl2  []string
+		want int
+	}{
+		{
+			name: "same lists", sl1: []string{"1", "2", "3"}, sl2: []string{"1", "2", "3"}, want: 0,
+		},
+		{
+			name: "different length1", sl1: []string{"1", "2", "3"}, sl2: []string{"1", "2", "3", "4"}, want: 1,
+		},
+		{
+			name: "different length2", sl1: []string{"1", "2", "3", "4"}, sl2: []string{"1", "2", "3"}, want: 1,
+		},
+		{
+			name: "same content, not same order", sl1: []string{"1", "2", "3"}, sl2: []string{"3", "2", "1"}, want: 0,
+		},
+		{
+			name: "different lists", sl1: []string{"1", "2", "3"}, sl2: []string{"4", "5", "6"}, want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CompareStringSlices(tt.sl1, tt.sl2); got != tt.want {
+				t.Errorf("CompareStringSlices() = %v, want %v", got, tt.want)
 			}
 		})
 	}
