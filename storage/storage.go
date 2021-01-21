f8b0112867dabf22ed7f017dbbcae67dca4aa4b8
eze-kiel <ezekiel@fastmail.com>
2021-01-21 17:59:09 +0100

    create custom key/value datastore

--- storage/storage.go
+++ storage/storage.go
@@ -1,7 +1,30 @@
 package storage
 
-// ListManager must implement those methods help us find differences with previous runs
-type ListManager interface {
-	ReadList(string) ([]string, error)
-	ReplaceList(string, []string) error
+// Store is a key/value
+type Store map[string][]string
+
+// Add a value to the store
+func (s Store) Add(k, v string) {
+	s[k] = append(s[k], v)
+}
+
+// Create initalize the store
+func Create() Store {
+	s := make(Store)
+	return s
+}
+
+// Delete a key from the store
+func (s Store) Delete(k string) {
+	delete(s, k)
+}
+
+// Get the values associated to a key from the store
+func (s Store) Get(k string) []string {
+	return s[k]
+}
+
+// Update a value in the store
+func (s Store) Update(k string, v []string) {
+	s[k] = v
 }
