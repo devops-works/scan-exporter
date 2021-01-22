c62023a9e2cd4991695a8e3948a5432ed7d5755a
eze-kiel <ezekiel@fastmail.com>
2021-01-22 15:01:12 +0100

    tests update

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -28,26 +28,3 @@ func Test_notFoundPage(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
-
-func Test_healthCheckPage(t *testing.T) {
-	req, err := http.NewRequest("GET", "/health", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(healthCheckPage)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
-}
