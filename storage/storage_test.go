ca586871a82acb34f6626ef1f7234f4ec1077030
ezekiel <ezekiel@fastmail.com>
2021-05-25 12:14:34 +0200

    wrote all tests for storage package

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -22,7 +23,7 @@ func TestStore_Get(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			output := tt.s.Get(tt.k)
 			if !equal(output, tt.expected) {
-				t.Errorf("got %q want %q given, %q", output, tt.expected, "test")
+				t.Errorf("got %q want %q given", output, tt.expected)
 			}
 		})
 	}
@@ -38,4 +39,106 @@ func equal(a, b []string) bool {
 		}
 	}
 	return true
+}
+func TestStore_Add(t *testing.T) {
+	tests := []struct {
+		name     string
+		s        Store
+		k        string
+		v        string
+		expected []string
+	}{
+		{
+			name:     "add value to non-existing key",
+			s:        map[string][]string{"toor": {"root"}},
+			k:        "foo",
+			v:        "bar",
+			expected: []string{"bar"},
+		},
+		{
+			name:     "add value to existing key",
+			s:        map[string][]string{"toor": {"root"}},
+			k:        "toor",
+			v:        "bar",
+			expected: []string{"root", "bar"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.Add(tt.k, tt.v)
+			if !equal(tt.s[tt.k], tt.expected) {
+				t.Errorf("got %q want %q given", tt.s[tt.k], tt.expected)
+			}
+		})
+	}
+}
+
+func TestStore_Delete(t *testing.T) {
+	tests := []struct {
+		name     string
+		s        Store
+		k        string
+		v        string
+		expected Store
+	}{
+		{
+			name:     "remove key",
+			s:        map[string][]string{"toor": {"root"}},
+			k:        "toor",
+			expected: map[string][]string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.Delete(tt.k)
+			if len(tt.s) != len(tt.expected) {
+				t.Errorf("got %q want %q given", tt.s, tt.expected)
+			}
+		})
+	}
+}
+
+func TestStore_Update(t *testing.T) {
+	tests := []struct {
+		name     string
+		s        Store
+		k        string
+		v        []string
+		expected []string
+	}{
+		{
+			name:     "update key",
+			s:        map[string][]string{"toor": {"root"}},
+			k:        "toor",
+			v:        []string{"bar"},
+			expected: []string{"bar"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.Update(tt.k, tt.v)
+			if !equal(tt.s[tt.k], tt.expected) {
+				t.Errorf("got %q want %q given", tt.s[tt.k], tt.expected)
+			}
+		})
+	}
+}
+
+func TestCreate(t *testing.T) {
+	tests := []struct {
+		name string
+		want Store
+	}{
+		{
+			name: "create basic store",
+			want: map[string][]string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Create(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
